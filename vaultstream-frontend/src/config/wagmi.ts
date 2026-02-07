1f4aaa8ebfee57462b7879edb34e514863060dbf
Neel Shah <neelshah@noreply.github.com>
2026-02-08 01:45:25 +0530

    Add WalletConnect project ID

--- vaultstream-frontend/src/config/wagmi.ts
+++ vaultstream-frontend/src/config/wagmi.ts
@@ -3,7 +3,7 @@ import { arbitrumSepolia } from 'wagmi/chains';
 
 export const config = getDefaultConfig({
   appName: 'VaultStream',
-  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || '',
+  projectId: '35328ad06930ca45c849babc50f7e5bd',
   chains: [arbitrumSepolia],
   ssr: true,
 });
